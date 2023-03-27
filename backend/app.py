9412df1b5efade8a3a96bc6c79ffc21ee8198eb0
Kartikey Subramanium <kartikey@tutanota.com>
2023-03-27 16:02:29 +0000

    Finally fix env

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY") or "super-secret-key"
-app.config["JWT_SECRET_KEY"] = os.environ("JWT_SECRET_KEY") or "super-secret-jwt-secret-key"
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY") or "super-secret-jwt-secret-key"
 jwt = JWTManager(app)
 
 maxSevSelDelay=1000
