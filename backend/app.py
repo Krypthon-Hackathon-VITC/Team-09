496e72d0868b26ccc0c1528c52cf774736634494
Kartikey Subramanium <kartikey@tutanota.com>
2023-03-27 16:00:54 +0000

    Fix debug secret keys

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from flask_jwt_extended import JWTManager
 from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"] or "super-secret-key"
-app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"] or "super-secret-jwt-secret-key"
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY") or "super-secret-key"
+app.config["JWT_SECRET_KEY"] = os.environ("JWT_SECRET_KEY") or "super-secret-jwt-secret-key"
 jwt = JWTManager(app)
 
 maxSevSelDelay=1000
