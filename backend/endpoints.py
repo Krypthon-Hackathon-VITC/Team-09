6ce4a69453e31ec03f457d3292472f16e10e36bf
Aditya Dutt <duttaditya18@gmail.com>
2023-03-28 07:25:21 +0530

    added pages /transfer and /statements

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,5 +1,5 @@
 import json
-from flask import jsonify, render_template, Response, request, make_response
+from flask import jsonify, render_template, Response, request, make_response, url_for, redirect
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, set_access_cookies
 import datetime
 from helper import *
@@ -46,18 +46,25 @@ def balance():
     username = json.loads(get_jwt_identity())["user"]
     return jsonify({'balance': get_balance(username)})
 
-@app.route("/statements", methods=("POST",))
+@app.route("/statements", methods=("POST", "GET"))
 @jwt_required()
 def statements():
     username = json.loads(get_jwt_identity())["user"]
     transactions = list(db["TRANSACTIONS"].find(
             {"$or": [{'FROM': username}, {'TO': username}]},
             {'_id': False}))
-    return jsonify({'transactions': transactions})
+    if request.method == 'POST':
+        return jsonify({'transactions': transactions})
+    elif request.method == 'GET':
+        return render_template("statements.html", transactions=transactions)
 
-@app.route("/transfer", methods=("POST",))
+@app.route("/transfer", methods=("GET", "POST"))
 @jwt_required()
 def transfer():
+    if request.method == 'GET':
+        success = request.args.get('success') or False
+        return render_template("transfer.html", success=success)
+
     user_from = json.loads(get_jwt_identity())["user"]
 
     user_to = request.form.get("user_to")
@@ -85,8 +92,7 @@ def transfer():
                         'AMOUNT': amount,
                         'REMARK': remark})
 
-    return Response(status=200)
-
+    return redirect(url_for('transfer', success=True))
 
 @app.route("/signup", methods=("POST",))
 def signup():
